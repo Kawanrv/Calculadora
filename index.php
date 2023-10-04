c293b5ff8ef1d1d76517631de4a361a529d6f0ad
Alison <alison.barreto@etec.sp.gov.br>
2023-10-04 20:36:50 -0300

    COMMIT by Kawanrv

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    include "form.php";
+    $_POST ($num1);
+    $_POST ($num2);
+    $_POST ($op);
+    if ($op == 1){
+        function calc($num1, $num2, $op){
+            return $num1 + $num2;
+        }
+    }if ($op == 2){
+
+    }if ($op == 3){
+
+    }if ($op == 4){
+        
+    }
+?>  
