80babd37636202bf37528b2e7d65b2ac5547647e
Kawan Rocha do Vale <131072945+Kawanrv@users.noreply.github.com>
2023-10-18 21:46:34 -0300

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,23 +1,32 @@
 <?php
-function soma($num1, $num2){
-	$total = $num1 + $num2;
-	return $total;
+if ($op = 1){
+	function soma($num1, $num2){
+		$total = $num1 + $num2;
+		return $num1;
+		return $num2;
+		echo $total;
+	}
+}else if ($op == 2){
+	function subtracao($num1, $num2){
+		$total = $num1 - $num2;
+		return $num1;
+		return $num2;
+		echo $total;
+	}
+}else if ($op == 3){
+	function divisao($num1, $num2){
+		$total = $num1 / $num2;
+		return $num1;
+		return $num2;
+		echo $total;
+	}
+}else if($op == 4){
+	function multiplicacao($num1, $num2){
+		$total = $num1 * $num2;
+		return $num1;
+		return $num2;
+		echo $total;
+	}
 }
-
-function subtracao($num1, num2)(
-	$total = $num1 - $num2;
-	return $total;
-)
-
-function divisao($num1, num2)(
-	$total = $num1 / $num2;
-	return $total;
-)
-
-function multiplicacao($num1, num2)(
-	$total = $num1 - $num2;
-	return $total;
-)
-
 ?>
 
